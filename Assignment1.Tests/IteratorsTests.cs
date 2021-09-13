3a2ea34ebc04b1e6d6e1d17b5dd91d36546eedfa
ehou <ehou@itu.dk>
2021-09-13 15:51:53 +0200

    Implemented and tested Iterators

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,41 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class IteratorsTests
     {
+        [Fact]
+        public void Return_Stream_Of_T_When_Given_Stream_Of_Stream_Of_T()
+        {
+            // Arrange
+            int[][] matrix = {
+                new [] {1, 3, 5, 7}, 
+                new [] {2, 4, 6, 8}
+            };
+            int[] expected = {1, 3, 5, 7, 2, 4, 6, 8};
+
+            // Act
+            IEnumerable<int> actual = Iterators.Flatten<int>(matrix);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        public void Returns_Only_Odd_Numbers_When_Given_Stream_Of_1to8_With_Odd_Predicate()
+        {
+            // Arrange
+            int[] array = {1, 3, 5, 7, 2, 4, 6, 8};
+            int[] expected = {1, 3, 5, 7};
+            
+            // Act
+            Predicate<int> odd = Iterators.Odd;
+            IEnumerable<int> actual = Iterators.Filter(array, odd);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -5,14 +5,35 @@ namespace Assignment1
 {
     public static class Iterators
     {
+        public static void Main(string[] args)
+        {
+            
+        }
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            foreach (var item in items)
+            {
+                foreach (var i in item)
+                {
+                    yield return i;
+                }
+            }
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static bool Odd(int i)
+        {
+            return i % 2 == 1;
         }
     }
 }
