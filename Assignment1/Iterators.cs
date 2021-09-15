2b1cadb5238c6731fe1225db023d180594b0d206
salsitu <90044151+salsitu@users.noreply.github.com>
2021-09-15 14:34:32 +0200

    Merge exercises (#2)
    * Add .gitignore
    
    * Implemented and tested Iterators
    
    * Refactored Odd()
    
    * Current status after first meeting. Work in progress
    
    * Tested and implemented InnerText().

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -7,12 +7,24 @@ namespace Assignment1
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            foreach (var item in items)
+            {
+                foreach (var i in item)
+                {
+                    yield return i;
+                }
+            }
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,23 +1,54 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
     public static class RegExpr
     {
+        public static void Main(){
+            
+        }
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            string regEx = @"[\s\r\n]+";
+
+            foreach (var line in lines)
+            {
+                var split = Regex.Split(line, regEx);
+                
+                foreach (var word in split)
+                {
+                    yield return word;
+                }
+            }
         }
 
-        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions)
         {
-            throw new NotImplementedException();
+            string regEx = @"(?<width>\d+)x(?<height>\d+)";
+
+            foreach (var resolution in resolutions)
+            {
+                var matches = Regex.Matches(resolution, regEx);
+
+                foreach (Match match in matches)
+                {
+                    GroupCollection groups = match.Groups;
+                    yield return (int.Parse(groups["width"].Value), int.Parse(groups["height"].Value));
+                }
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
         {
-            throw new NotImplementedException();
+            string regEx = string.Format(@"<([{0}][^>]*)>(?<innerText>.+?)</{0}>", tag);
+
+            foreach (Match match in Regex.Matches(html, regEx))
+            {
+                GroupCollection group = match.Groups;
+                yield return Regex.Replace(group["innerText"].Value, "<[^>]*>", "");
+            }
         }
     }
 }
